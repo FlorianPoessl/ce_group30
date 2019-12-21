e0b8a1223596e627fb11aade0c9a9ed3da211bae
FlorianPoessl <florian.poessl97@gmail.com>
2019-12-21 14:49:42 +0100

    all html docs

--- src/main/java/ce/group30/fahrradlenker/controller/FahrradlenkerController.java
+++ src/main/java/ce/group30/fahrradlenker/controller/FahrradlenkerController.java
@@ -7,7 +7,34 @@ import org.springframework.web.servlet.ModelAndView;
 @RestController
 public class FahrradlenkerController {
     @RequestMapping("/index")
-    public ModelAndView myindex(){
+    public ModelAndView getIndex(){
         return new ModelAndView("index");
     }
+
+    @RequestMapping("/customer")
+    public ModelAndView getCustomer(){
+        return new ModelAndView("customer");
+    }
+
+    @RequestMapping("/gearing")
+    public ModelAndView getGearing(){
+        return new ModelAndView("gearing");
+    }
+
+    @RequestMapping("/handle")
+    public ModelAndView getHandle(){
+        return new ModelAndView("handle");
+    }
+
+    @RequestMapping("/material")
+    public ModelAndView getMaterial(){
+        return new ModelAndView("material");
+    }
+
+    @RequestMapping("/order")
+    public ModelAndView getOrder(){
+        return new ModelAndView("order");
+    }
+
+
 }
